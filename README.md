2372b937c1e6b20fc08f57cfad9d0e3c1d935bd0
wwLoveWj <blww885@163.com>
2024-05-14 15:40:32 +0800

    feat: 查询sql上传

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-### 创建页面 
- > umi g page Dashboard/Monitor   //默认生成的文件后缀是js和css
-   umi g page Dashboard/Monitor --typescript --less   //显示指定生成的文件后缀是tsx和less
+### 创建页面
+
+> umi g page Dashboard/Monitor //默认生成的文件后缀是js和css
+> umi g page Dashboard/Monitor --typescript --less //显示指定生成的文件后缀是tsx和less
+
+1. 后端工具使用的是Database Client工具
--- src/pages/rules/CreateMoney.tsx
+++ src/pages/rules/CreateMoney.tsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
-import {getMoneySvg} from "@/utils/request/api/rule.ts";
+import { getMoneySvg } from "@/utils/request/api/rule.ts";
 import styles from "./style.less";
 
 const Index = () => {
-  const [show ,setShow] =useState<any>("44");
+  const [show, setShow] = useState<any>("");
 
-  const getMoneySvgApi =async ()=>{
+  const getMoneySvgApi = async () => {
     let res = await getMoneySvg({});
     var parser = new DOMParser();
     var xmlDoc = parser.parseFromString(res, "text/html");
-    setShow(res)
-  }
-  useEffect(()=>{
-    getMoneySvgApi()
-  },[])
+    setShow(res);
+  };
+  useEffect(() => {
+    getMoneySvgApi();
+  }, []);
   return (
     <div className={styles.money}>
-      <div dangerouslySetInnerHTML={{__html:show}}></div>
+      <div dangerouslySetInnerHTML={{ __html: show }}></div>
     </div>
   );
 };
